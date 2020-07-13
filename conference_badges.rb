d660bf7fe26085e93504cb1c9e6a41a3a1ce91e4
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-13 20:49:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,13 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-def batch_badge_creator(speakers)
-  names.each do |speaker|
-    puts "Hello, my name is #{speaker}."
+def batch_badge_creator(attendees)
+  attendee.each do |attendee|
+    puts "Hello, my name is #{attendee}."
   end
 end
+
+def assign_rooms 
+  assign_rooms.map.with-index |attendee,i| attendee +=1
