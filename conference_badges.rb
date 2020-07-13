5779f5bab9287ad455777b87160643aa62410325
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-13 23:05:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,6 +20,6 @@ room_assignments
 end
 
 def printer(attendees)
- batch_badge_creator(attendees)
- assign_rooms(attendees)
+ puts "batch_badge_creator(attendees)"
+ puts "assign_rooms(attendees)"
 end
