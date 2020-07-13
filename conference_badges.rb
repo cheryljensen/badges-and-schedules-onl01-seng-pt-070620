baf7264222ce9c2275336fe12ae95f2760192971
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-13 21:04:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |attendee|
-    badges<<"Hello, my name is #{name}."
+    badges<<"Hello, my name is #{attendee}."
   end
 badges
 end
