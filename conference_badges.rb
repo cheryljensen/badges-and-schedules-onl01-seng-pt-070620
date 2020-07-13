531081c678b864c075b6393a1dcc3412ce7b2f6b
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-13 20:41:24 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,9 @@
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(speakers)
+  names.each do |speaker|
+    puts "Hello, my name is #{speaker}."
+end
