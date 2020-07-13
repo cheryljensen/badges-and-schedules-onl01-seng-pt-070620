84dd52b479a4df1f49839d14bf74f69b98dc0be2
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-13 22:32:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each do |attendee, index|
-  room_assignments<<"Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
+  room_assignments<<"Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 room_assignments
 end
