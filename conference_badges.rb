a0aad47ed836dd6988706185825e20799abdc0d6
Cheryl Jensen <cherylbjensen@gmail.com>
2020-07-14 00:02:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |attendee|
-    badges<<"Hello, my name is #{attendee}."
+    badges << "Hello, my name is #{attendee}."
   end
 badges
 end
@@ -14,12 +14,17 @@ end
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |attendee, index|
-  room_assignments<<"Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+  room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
 end
 room_assignments
 end
 
 def printer(attendees)
- puts batch_badge_creator(attendees)
- puts assign_rooms(attendees)
+ badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+ rooms = assign_rooms(attendees)
+ rooms.each do |room|
+  puts room
 end
